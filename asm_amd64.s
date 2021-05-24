479730637731faebc68b4038d508f3f65f34320c
zhangyunhao <zhangyunhao116@gmail.com>
2021-05-24 15:51:13 +0800

    wip

--- asm_amd64.s
+++ asm_amd64.s
@@ -3,24 +3,24 @@
 #include "textflag.h"
 
 TEXT ·compareAndSwapUint128(SB),NOSPLIT,$0
-	MOVQ addr+0(FP), BP
+	MOVQ addr+0(FP), R8
 	MOVQ old+8(FP), AX
 	MOVQ old+16(FP), DX
 	MOVQ new+24(FP), BX
 	MOVQ new+32(FP), CX
 	LOCK
-	CMPXCHG16B (BP)
+	CMPXCHG16B (R8)
 	SETEQ swapped+40(FP)
 	RET
 
 TEXT ·loadUint128(SB),NOSPLIT,$0
-	MOVQ addr+0(FP), BP
+	MOVQ addr+0(FP), R8
 	XORQ AX, AX
 	XORQ DX, DX
 	XORQ BX, BX
 	XORQ CX, CX
 	LOCK
-	CMPXCHG16B (BP)
+	CMPXCHG16B (R8)
 	MOVQ AX, val+8(FP)
 	MOVQ DX, val+16(FP)
 	RET
