5daeb8ea8aeb591d6028cf565cbd8f918b16c454
zhangyunhao <zhangyunhao116@gmail.com>
2021-06-20 17:27:05 +0800

    .

--- cache.go
+++ cache.go
@@ -1,28 +1,5 @@
 package lscq
 
-func cacheRemap(index, slicecap int) int {
-	// return index
-	return cacheRemap2(index, slicecap)
-}
-
-func cacheRemap2(index, slicecap int) int {
-	if slicecap <= int(cacheLineSize) {
-		return index
-	}
-	cacheLineNum := (index % int(cacheLineSize)) % (slicecap / int(cacheLineSize))
-	idx := index / (slicecap / int(cacheLineSize))
-	res := cacheLineNum*int(cacheLineSize) + idx
-	return res
-}
-
-var indexTable [scqsize]uint64
-
-func init() {
-	for i := 0; i < scqsize; i++ {
-		indexTable[i] = cacheRemap16Byte(uint64(i))
-	}
-}
-
 func cacheRemap16Byte(index uint64) uint64 {
 	const cacheLineSize = cacheLineSize / 2
 	rawIndex := index & uint64(scqsize-1)
